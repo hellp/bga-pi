0b53b945c14a20967f910e8750f5de87f0473fb2
Fabian Neumann <mail@fabianneumann.de>
2018-01-25 17:17:24 +0100

    The big Renaming

--- states.inc.php
+++ states.inc.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * fabiantest implementation: © Fabian Neumann <fabian.neumann@posteo.de>
+ * P.I. implementation: © Fabian Neumann <fabian.neumann@posteo.de>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
