d874a265161084868e714b8be22a7980004a736f
Fabian Neumann <mail@fabianneumann.de>
2018-05-15 23:29:19 +0200

    Add a Polyfill for String.startsWith (for IE11). Fixes BGA bugs #9943, #9918

--- pi.js
+++ pi.js
@@ -15,6 +15,14 @@
  *
  */
 
+// Polyfill for IE11
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(search, pos) {
+       return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+    };
+}
+
 define([
     "dojo",
     "dojo/_base/declare",
